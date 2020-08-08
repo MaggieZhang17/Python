718617638022c4666a4f850c4218ac871bce6819
Maggie Zhang <69331410+MaggieZhang17@users.noreply.github.com>
2020-08-08 22:51:52 +0800

    Update 25import模块.py

--- python3_tutorials_莫凡/25import模块.py
+++ python3_tutorials_莫凡/25import模块.py
@@ -2,12 +2,12 @@
 
 # import time
 
-# import time as t
+# import time as t -- 简单写法 以后就是t.*** 而不是time.***
 
 
-# from time import time,localtime
-
-from time import *
+# from time import localtime -- 以后就简写写localtime
+ 
+from time import *    #把所有功能都简写了
 
 
 print(localtime())
